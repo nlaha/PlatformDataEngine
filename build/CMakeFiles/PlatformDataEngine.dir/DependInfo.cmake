acb139ba607465890c743511799514a6042ec05d
jaxon-mcclelland <jmcclelland1844@protonmail.com>
2022-04-12 17:51:09 -0700

    Added Cmake functionality. Build still broken

---
+++ build/CMakeFiles/PlatformDataEngine.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jaxon/Projects/WSU/CPTS_122/PA9/PlatformDataEngine/PlatformDataEngine/main.cpp" "/home/jaxon/Projects/WSU/CPTS_122/PA9/PlatformDataEngine/build/CMakeFiles/PlatformDataEngine.dir/PlatformDataEngine/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "FMT_LOCALE"
+  "JSON_DIAGNOSTICS=0"
+  "JSON_USE_IMPLICIT_CONVERSIONS=1"
+  "SFML_STATIC"
+  "SPDLOG_COMPILED_LIB"
+  "SPDLOG_FMT_EXTERNAL"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../vcpkg_installed/x64-linux/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/PlatformDataEngine.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PlatformDataEngine.dir/PlatformDataEngine/main.cpp.o"
+  "PlatformDataEngine"
+  "PlatformDataEngine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PlatformDataEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

dd5bee500ea88cd69fa7e7a1f07c86c06ca09b00
Nathan Laha <lahan@uw.edu>
2022-04-16 17:06:42 -0700

    new map and tileset

---
+++ PlatformDataEngine/assets/SpaceTilesetOvergrown.tsx
@@ -0,0 +1,369 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="SpaceTilesetOvergrown" tilewidth="8" tileheight="8" tilecount="64" columns="16">
+ <image source="SpaceTilesetOvergrown.png" width="128" height="32"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="8" y="8">
+    <polygon points="0,0 0,-4 -2,-4 -4,-2 -4,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0" y="4">
+    <polygon points="0,0 8,0 8,4 0,4"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="5">
+   <object id="3" x="0" y="4">
+    <polygon points="0,0 8,0 8,4 0,4"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="4">
+    <polygon points="0,0 2,0 4,2 4,4 0,4"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 8,0 8,8 0,8"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 4,0 4,8 0,8"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="0">
+    <polygon points="0,0 4,0 4,8 0,8"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 8,0 8,8 0,8 0,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="0">
+    <polygon points="0,0 0,8 4,8 4,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="10" probability="0.2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 4,0 4,8 0,8"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="11" probability="0.5"/>
+ <tile id="14" probability="0.2"/>
+ <tile id="15" probability="0.5"/>
+ <tile id="16">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="0">
+    <polygon points="0,0 4,0 4,8 0,8"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 8,0 8,4 6,4 4,6 4,8 0,8"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="18">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 8,0 8,8 4,8 4,6 2,4 0,4"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="19">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 4,0 4,8 0,8"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="20">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 8,0 8,4 0,4"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="21">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 8,0 8,8 0,8"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="22">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 8,0 8,8 0,8"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="23" probability="0.5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,4 8,4 8,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="24">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="4">
+    <polygon points="0,0 8,0 8,4 0,4"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="25">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="4" y="0">
+    <polygon points="0,0 0,2 -2,4 -4,4 -4,8 4,8 4,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="26">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 4,0 4,2 6,4 8,4 8,8 0,8"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="27">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="4">
+    <polygon points="0,0 8,0 8,4 0,4"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="31" probability="0.5"/>
+ <tile id="32">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="0">
+    <polygon points="0,0 4,0 4,8 0,8"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="33">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="0" y="0">
+    <polygon points="0,0 0,8 8,8 8,4 6,4 4,2 4,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="34">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="0">
+    <polygon points="0,0 4,0 4,8 -4,8 -4,4 -2,4 0,2"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="35">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 4,0 4,8 0,8"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="36">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="4">
+    <polygon points="0,0 8,0 8,4 0,4"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="37">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 8,0 8,8 0,8"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="38">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 8,0 8,8 0,8"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="39">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="4">
+    <polygon points="0,0 8,0 8,4 0,4"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="40">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 8,0 8,4 0,4"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="41">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 8,0 8,8 4,8 4,6 2,4 0,4"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="42">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 8,0 8,4 6,4 4,6 4,8 0,8"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="43">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 8,0 8,4 0,4"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="48">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="4" y="0">
+    <polygon points="0,0 4,0 4,4 2,4 0,2"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="49">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 8,0 8,4 0,4"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="50">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 8,0 8,4 0,4"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="51">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="0" y="0">
+    <polygon points="0,0 4,0 4,2 2,4 0,4"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="52">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 0,8 8,8 8,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="53">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 4,0 4,8 0,8"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="54">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="0">
+    <polygon points="0,0 0,8 4,8 4,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="55">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 8,0 8,8 0,8"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="57">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="0">
+    <polygon points="0,0 0,8 4,8 4,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="58">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0">
+    <polygon points="0,0 4,0 4,8 0,8"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <wangsets>
+  <wangset name="SpaceAndGround" type="corner" tile="-1">
+   <wangcolor name="Ground" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,0,0,1,0,0,0,0"/>
+   <wangtile tileid="1" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="2" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="3" wangid="0,0,0,0,0,1,0,0"/>
+   <wangtile tileid="5" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="6" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="7" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="9" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="10" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="16" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="17" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="18" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="19" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="20" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="21" wangid="0,0,0,1,0,0,0,1"/>
+   <wangtile tileid="22" wangid="0,1,0,0,0,1,0,0"/>
+   <wangtile tileid="23" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="24" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="25" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="26" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="27" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="32" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="33" wangid="0,0,0,1,0,1,0,1"/>
+   <wangtile tileid="34" wangid="0,1,0,1,0,1,0,0"/>
+   <wangtile tileid="35" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="36" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="37" wangid="0,1,0,0,0,1,0,0"/>
+   <wangtile tileid="38" wangid="0,0,0,1,0,0,0,1"/>
+   <wangtile tileid="39" wangid="0,0,0,1,0,1,0,0"/>
+   <wangtile tileid="40" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="41" wangid="0,1,0,1,0,0,0,1"/>
+   <wangtile tileid="42" wangid="0,1,0,0,0,1,0,1"/>
+   <wangtile tileid="43" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="48" wangid="0,1,0,0,0,0,0,0"/>
+   <wangtile tileid="49" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="50" wangid="0,1,0,0,0,0,0,1"/>
+   <wangtile tileid="51" wangid="0,0,0,0,0,0,0,1"/>
+   <wangtile tileid="53" wangid="0,0,0,0,0,1,0,1"/>
+   <wangtile tileid="54" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="57" wangid="0,1,0,1,0,0,0,0"/>
+   <wangtile tileid="58" wangid="0,0,0,0,0,1,0,1"/>
+  </wangset>
+  <wangset name="SpaceBG" type="corner" tile="-1">
+   <wangcolor name="Space" color="#ff0000" tile="-1" probability="1"/>
+   <wangtile tileid="14" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="15" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="30" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="31" wangid="0,1,0,1,0,1,0,1"/>
+  </wangset>
+ </wangsets>
+</tileset>
